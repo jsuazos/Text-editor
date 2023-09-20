171763fd3554e5c47de9ea4bd3f19eb36c39556b
jsuazo <javier2587@gmail.com>
2023-09-20 16:35:10 -0300

    commit word count

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ smileys = [
     'other',[':3']
 ]
 
+smiley_events = smileys[1] + smileys[3] + smileys[5]
+
 menu_layout = [
     ['File', ['Open', 'Save', '---', 'Exit']],
     ['Tools', ['Word count']],
     ['Add', smileys]
 ]
 
+sg.theme('GrayGrayGray')
+
 layout = [
     [sg.Menu(menu_layout)],
     [sg.Text('Untitled', key = '-DOCNAME-')],
-    [sg.Multiline()]
+    [sg.Multiline(
+        no_scrollbar = True, 
+        size = (50, 30), 
+        key = '-TEXTBOX-')]
 ]
 
 window = sg.Window('Text editor', layout)
@@ -25,5 +32,13 @@ while True:
 
     if event == sg.WINDOW_CLOSED:
         break
+
+    if event == 'Word count':
+        full_text = values['-TEXTBOX-']
+        clean_text = full_text.replace('\n', ' ').split(' ')
+        word_count = len(clean_text)
+        char_count = len(''.join(clean_text))
+        sg.popup(f'Palabras: {word_count}\nCaracteres: {char_count}')
+        
 
 window.close()
